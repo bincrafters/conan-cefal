76664bba37f35c52f85e108bd7c1310304248e7a
Uilian Ries <uilianries@gmail.com>
2020-04-30 16:19:52 -0300

    Add cppstd
    Signed-off-by: Uilian Ries <uilianries@gmail.com>

--- conanfile.py
+++ conanfile.py
@@ -1,4 +1,5 @@
 from conans import ConanFile, tools, CMake
+from conans.errors import ConanInvalidConfiguration
 import os
 
 
@@ -8,10 +9,32 @@ class CefalConan(ConanFile):
     topics = ("conan", "cefal", "monad")
     url = "https://github.com/bincrafters/conan-cefal"
     homepage = "https://github.com/dkormalev/cefal"
-    license = "BSD-3-Clause"  # Indicates license type of the packaged library; please use SPDX Identifiers https://spdx.org/licenses/
+    license = "BSD-3-Clause"
+    settings = "compiler"
     no_copy_source = True
 
     _source_subfolder = "source_subfolder"
+
+    def configure(self):
+        minimal_cpp_standard = "20"
+        if self.settings.compiler.cppstd:
+            tools.check_min_cppstd(self, minimal_cpp_standard)
+        minimal_version = {
+            "gcc": "10",
+            "clang": "10",
+            "apple-clang": "10",
+            "Visual Studio": "16"
+        }
+        compiler = str(self.settings.compiler)
+        if compiler not in minimal_version:
+            self.output.warn(
+                "%s recipe lacks information about the %s compiler standard version support." % (self.name, compiler))
+            self.output.warn(
+                "%s requires a compiler that supports at least C++%s." % (self.name, minimal_cpp_standard))
+            return
+        version = tools.Version(self.settings.compiler.version)
+        if version < minimal_version[compiler]:
+            raise ConanInvalidConfiguration("%s requires a compiler that supports at least C++%s." % (self.name, minimal_cpp_standard))
 
     def source(self):
         tools.get(**self.conan_data["sources"][self.version])
@@ -19,10 +42,11 @@ class CefalConan(ConanFile):
         os.rename(extracted_dir, self._source_subfolder)
 
     def package(self):
+        self.copy("LICENSE", dst="licenses", src=self._source_subfolder)
         cmake = CMake(self)
         cmake.configure(source_folder=self._source_subfolder)
         cmake.install()
-        self.copy("LICENSE", dst="licenses", src=self._source_subfolder)
+        tools.rmdir(os.path.join(self.package_folder, "lib"))
 
     def package_id(self):
         self.info.header_only()
